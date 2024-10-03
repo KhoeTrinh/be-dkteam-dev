93bec33bc3ed7501a33882e861b375800e1660e2
KhoeTrinh <kelvintrinh09@gmail.com>
2024-10-03 09:23:37 +0700

    done specifying users route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    // For users & Admin
+    @Get('/')
+    Check() {}
+
+    @Post('/login')
+    Login() {}
+
+    @Post('/signup')
+    Signup() {}
+
+    @Post('/logout')
+    Logout() {}
+
+    @Put('/:id')
+    UpdateById() {}
+
+    @Delete('/:id')
+    DeleteById() {}
+
+    // For Admin
+    @Get('/admin')
+    AllUsers() {}
+
+    @Get('/:id/admin')
+    UserById() {}
+
+    @Put('/:id/admin')
+    UpdateByIdAdmin() {}
+
+    @Delete('/:id/admin')
+    DeleteByIdAdmin() {}
+}
