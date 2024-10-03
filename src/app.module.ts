c29a3c5c695e4c59065819c0755d166ef5ffa6b3
KhoeTrinh <kelvintrinh09@gmail.com>
2024-10-03 10:22:00 +0700

    done prisma plugin

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [UsersModule, ProductsModule, PrismaModule],
   controllers: [],
   providers: [],
 })
