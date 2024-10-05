57133680a9533c16b744a87beb1bab7a43ed9ebc
KhoeTrinh <kelvintrinh09@gmail.com>
2024-10-05 15:49:35 +0700

    add comment to prisma model and add comments mo-co-s

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { CommentsModule } from './comments/comments.module';
 
 
 @Module({
-  imports: [UsersModule, ProductsModule, PrismaModule],
+  imports: [UsersModule, ProductsModule, PrismaModule, CommentsModule],
   controllers: [],
   providers: [],
 })
