d8b3f57db51b23efdfcfa6c1db0b0728c4e39df2
KhoeTrinh <kelvintrinh09@gmail.com>
2024-11-06 08:01:56 +0700

    vlll

--- src/github-image/github-image.module.ts
+++ src/github-image/github-image.module.ts
@@ -4,11 +4,13 @@ import { GithubImageController } from './github-image.controller';
 import { HttpModule } from '@nestjs/axios';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [
     HttpModule,
     PrismaModule,
+    UsersModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '1d' },
